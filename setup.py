8a61a1c1ebf23d8b8e61b8e5b2dd3b1dcbcb7b20
m081429 <naresh.prodduturi@gmail.com>
2020-01-02 14:31:45 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tools to aid Digital pathology deep learning projects',
+    author='Naresh Prodduturi',
+    license='MIT',
+)
